6df0b0658f08dfb87bfb96fe489f7d661be7380d
Franz Schnyder <schnyderfranz@gmail.com>
2012-01-16 22:43:52 +0100

    First working version of Com::TcpServer

--- ArduinoCore/source/main/inc/Bt/Com/TcpServer.hpp
+++ ArduinoCore/source/main/inc/Bt/Com/TcpServer.hpp
@@ -12,17 +12,18 @@
 #define INC__Bt_Com_TcpServer__hpp
 
 #include "I_RequestServer.hpp"
-#include "EthernetServer.h"
-#include "EthernetClient.h"
+#include "Socket.h"
 #include "StaticArrayPackageBuffer.hpp"
 #include "BinaryInputPackage.hpp"
 #include "BinaryOutputPackage.hpp"
+
+#include <Arduino.h>
 
 namespace Bt {
 namespace Com {
 
 
-
+template<typename ServerSocket, typename Socket>
 class TcpServer 
 {
    public:
@@ -31,7 +32,7 @@ class TcpServer
       enum { TCP_SERVER_CHECK_BYTE    = 0xAF};
 
 
-      TcpServer(EthernetServer& iServerSocket, I_RequestServer& iHandler);
+      TcpServer(ServerSocket& iServerSocket, I_RequestServer& iHandler);
       ~TcpServer();
    
       virtual void workcycle();
@@ -51,10 +52,10 @@ class TcpServer
       size_t calculateReadSize(size_t iAvailable);
       bool checkConnection();
 
-      typedef void (TcpServer::*StateFunction)();
+      typedef void (TcpServer<ServerSocket,Socket>::*StateFunction)();
 
-      EthernetServer* mServerSocket;
-      EthernetClient mSocket;
+      ServerSocket* mServerSocket;
+      Socket mSocket;
       StateFunction mStateFunction;
       I_RequestServer* mHandler;
 
@@ -67,6 +68,156 @@ class TcpServer
 
 
 };
+
+//-------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+TcpServer<ServerSocket,Socket>::TcpServer(ServerSocket& iServerSocket, I_RequestServer& iHandler)
+: mServerSocket(&iServerSocket), mStateFunction(&TcpServer<ServerSocket,Socket>::waitForClient), mHandler(&iHandler), mSize(0) , mInput(mInputBuffer), mOutput(mOutputBuffer) {
+
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+TcpServer<ServerSocket,Socket>::~TcpServer() {
+
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+void TcpServer<ServerSocket,Socket>::workcycle(void) {
+   (this->*mStateFunction)();
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+void TcpServer<ServerSocket,Socket>::waitForClient() {
+   mSocket = mServerSocket->available();
+   if (mSocket == true) {
+      mStateFunction = &TcpServer<ServerSocket,Socket>::waitForRequest;
+      workcycle();
+   }
+}
+
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+void TcpServer<ServerSocket,Socket>::waitForRequest() {
+   if (mSocket.available() <= 0) {
+      checkConnection();
+      return;
+   }
+
+   int checkByte = mSocket.read();
+   if (checkByte == -1) {
+      checkConnection();
+      return;
+   }
+
+   if (checkByte != TCP_SERVER_CHECK_BYTE) {
+      Serial.print("wrong check byte ");
+      Serial.print(checkByte,HEX);
+      Serial.print(" != ");
+      Serial.print(TCP_SERVER_CHECK_BYTE,HEX);
+      Serial.println(" => close connection");
+      mSocket.stop();
+      mStateFunction = &TcpServer<ServerSocket,Socket>::waitForClient;
+      return;
+   }
+   mStateFunction = &TcpServer<ServerSocket,Socket>::readSize;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+void TcpServer<ServerSocket,Socket>::readSize() {
+
+   if (mSocket.available() <= 0) {
+      checkConnection();
+      return;
+   }
+
+   int size = mSocket.read();
+   if (size == -1) {
+      checkConnection();
+      return;
+   }
+
+   mSize = size;
+
+   mInputBuffer.clear();
+   mStateFunction = &TcpServer<ServerSocket,Socket>::readData;
+}
+
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+void TcpServer<ServerSocket,Socket>::readData() {
+   int available = mSocket.available();
+   if (available <= 0) {
+      checkConnection();
+      return;
+   }
+
+   size_t calculatedReadSize = calculateReadSize(available);
+   int read = mSocket.read(mInputBuffer.raw() + mInputBuffer.length(),calculatedReadSize);
+   if (read == -1) {
+      checkConnection();
+      return;
+   }
+
+   mInputBuffer.filled(mInputBuffer.length() + read);
+
+   if(mInputBuffer.length() < mSize) {
+      return;
+   }
+
+   mOutputBuffer.clear();
+
+   mHandler->handleRequest(mInput,mOutput);
+
+   mSocket.write(TCP_SERVER_CHECK_BYTE);
+   mSocket.write(mOutputBuffer.length());
+   if (mOutputBuffer.length() > 0) {
+      mSocket.write(mOutputBuffer.raw(),mOutputBuffer.length());
+   }
+
+   mStateFunction = &TcpServer<ServerSocket,Socket>::waitForRequest;
+
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+size_t TcpServer<ServerSocket,Socket>::calculateReadSize(size_t iAvailable) {
+   size_t remaining = mSize - mInputBuffer.length();
+   if (remaining < iAvailable) {
+      return remaining;
+   }
+   return iAvailable;
+}
+
+//-------------------------------------------------------------------------------------------------
+
+template<typename ServerSocket, typename Socket>
+bool TcpServer<ServerSocket,Socket>::checkConnection() {
+   if (!mSocket.connected()) {
+      Serial.println("connection lost => restart socket");
+      mSocket.flush();
+      mSocket.stop();
+      mStateFunction = &TcpServer<ServerSocket,Socket>::waitForClient;
+      return false;
+   }
+   return true;
+}
+
+//-------------------------------------------------------------------------------------------------
 
 
 } // namespace Com
--- ArduinoScreenMaster/source/main/src/Main.cpp
+++ ArduinoScreenMaster/source/main/src/Main.cpp
@@ -11,36 +11,29 @@
 #include <Arduino.h>
 #include <Bt/Ui/Colorduino.hpp>
 #include <Bt/Ui/RgbScreenProxy.hpp>
+#include <Bt/Ui/RgbScreenServer.hpp>
 #include <Bt/Ui/CompoundRgbScreen.hpp>
 #include <Bt/Com/TwoWireClient.hpp>
+#include <Bt/Com/TcpServer.hpp>
 #include <Bt/Com/Twi.hpp>
+#include "EthernetServer.h"
+#include "EthernetClient.h"
+#include "Ethernet.h"
 
 static const unsigned long STARTUP_DELAY = 3000 ; // 3s
 
 
 int main() {
 
-
    init();
 
-   //Serial.begin(9600);
+   Serial.begin(9600);
+
+   Serial.println("Startup");
 
    delay(STARTUP_DELAY);
 
-   Bt::Com::Twi twi;
-
-   Bt::Com::TwoWireClient<Bt::Com::Twi> server1(twi,1);
-   Bt::Com::TwoWireClient<Bt::Com::Twi> server2(twi,2);
-
-   Bt::Ui::RgbScreenProxy proxy1(server1);
-   Bt::Ui::RgbScreenProxy proxy2(server2);
-   Bt::Util::StaticMatrix<Bt::Ui::I_RgbScreen*,1,2> screens;
-   screens(0,0) = &proxy1;
-   screens(0,1) = &proxy2;
-
-   Bt::Ui::CompoundRgbScreen screen(screens);
-
-
+   Serial.println("Colors");
 
    Bt::Ui::Color black     (  0,  0,  0);
    Bt::Ui::Color red       (255,  0,  0);
@@ -49,103 +42,61 @@ int main() {
    Bt::Ui::Color yellow    (255,255,  0);
    Bt::Ui::Color white     (255,255,255);
 
-   /*Bt::Ui::Color colors[] = {
-      Bt::Ui::Color(255,255,255),
-      Bt::Ui::Color(  0,  0,255),
-      Bt::Ui::Color(  0,255,  0),
-      Bt::Ui::Color(255,  0,  0),
-      Bt::Ui::Color(  0,  0,  0)
-   };
-   */
+   Serial.println("Twi");
+   Bt::Com::Twi twi;
+   Bt::Com::TwoWireClient<Bt::Com::Twi> server1(twi,1);
+   Bt::Com::TwoWireClient<Bt::Com::Twi> server2(twi,2);
 
+   Serial.println("Twi Screens");
+   Bt::Ui::RgbScreenProxy proxy1(server1);
+   Bt::Ui::RgbScreenProxy proxy2(server2);
+   Bt::Util::StaticMatrix<Bt::Ui::I_RgbScreen*,1,2> screens;
+   screens(0,0) = &proxy1;
+   screens(0,1) = &proxy2;
 
-   int d = 10;
+   Serial.println("Compound");
+   Bt::Ui::CompoundRgbScreen screen(screens);
+   screen.fill(red);
+   screen.repaint();
 
-   proxy1.fill(green);
-   proxy1.repaint();
-   proxy2.fill(red);
-   proxy2.repaint();
-   delay(d);
-   proxy1.fill(black);
-   proxy1.repaint();
-   proxy2.fill(black);
-   proxy2.repaint();
-   delay(d);
+   Serial.println("RgbServer");
+   Bt::Ui::RgbScreenServer server(screen);
 
-   while(true) {
-      for (size_t y = 0; y < screen.height(); ++y) {
-         for (size_t x = 0; x < screen.width(); ++x) {
-            screen.setPixel(x,y,green);
-            screen.repaint();
-            delay(d);
-         }
-      }
-      for (size_t y = 0; y < screen.height(); ++y) {
-         for (size_t x = 0; x < screen.width(); ++x) {
-            screen.setPixel(x,y,blue);
-         }
-      }
-      screen.repaint();
+   byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 };
 
+   Serial.print("DHCP");
+   while (Ethernet.begin(mac) == 0) {
+      Serial.println("Failed to configure Ethernet using DHCP ...");
+      Serial.println(" ... wait for a retry ...");
       delay(1000);
-
-      screen.fill(black);
-      screen.repaint();
-      delay(d);
-      for (size_t i = 0 ; i < screen.width() ; ++i) {
-         screen.setPixel(i,0,green);
-         screen.repaint();
-         delay(d);
-      }
-      for (size_t i = 0 ; i < screen.height() ; ++i) {
-         screen.setPixel(i,i,red);
-         screen.repaint();
-         delay(d);
-      }
-      for (size_t i = 0 ; i < screen.height() ; ++i) {
-         screen.setPixel(screen.width()/2+i,screen.height()-1-i,red);
-         screen.repaint();
-         delay(d);
-      }
-
-      for (size_t i = 0; i < screen.height() ; ++i) {
-         screen.setPixel(0,screen.height()-1-i,blue);
-         screen.repaint();
-         delay(d);
-      }
-
-      for (size_t i = 0; i < screen.height() ; ++i) {
-         screen.setPixel(screen.width()-1,screen.height()-1-i,blue);
-         screen.repaint();
-         delay(d);
-      }
-
-
-      for (int l = screen.height()-1; l > 2; --l) {
-         for (size_t i = 0 ; i < screen.width() ; ++i) {
-            screen.setPixel(screen.width()-1-i,l,yellow);
-         }
-      }
-
-      screen.repaint();
-      delay(d);
-
-      delay(1000);
+      Serial.println(" ... retry");
    }
 
-
-
-   /*
-   while(true) {
-      for (unsigned int i = 0; i < sizeof(colors)/sizeof(colors[0]) ; ++i) {
-         proxy1.fill(colors[i]);
-         proxy2.fill(colors[i]);
-         proxy1.repaint();
-         proxy2.repaint();
-         delay(d);
-      }
+   Serial.print("My IP address: ");
+   IPAddress ip = Ethernet.localIP();
+   for (byte thisByte = 0; thisByte < 4; thisByte++) {
+      // print the value of each byte of the IP address:
+      Serial.print(ip[thisByte], DEC);
+      Serial.print(".");
    }
-   */
+   Serial.println();
+
+   screen.fill(yellow);
+   screen.repaint();
+
+   Serial.println("EthernetServer");
+   EthernetServer ethernetServer(2000);
+   ethernetServer.begin();
+
+   Bt::Com::TcpServer<EthernetServer,EthernetClient> tcpServer(ethernetServer,server);
+
+   screen.fill(green);
+   screen.repaint();
+
+   Serial.println("workcycle");
+   while(true) {
+      tcpServer.workcycle();
+   }
 
    return 0;
 }
